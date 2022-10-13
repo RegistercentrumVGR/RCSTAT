d913f0d9e950fecf013be9de744543831658890e
Rikard Isaksson <rikard.isaksson@vgregion.se>
2022-10-13 12:31:57 +0200

    2:nd initial commit!!

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,7 +1,6 @@
 test_that("multiplication works", {
   df <- dummy_data
 
-  ageint <- mutate(df, age_int(adate, bdate))
-  age <- mutate(df, age(adate, bdate))
+  age <- mutate(df, age = age(adate, bdate))
 
 })

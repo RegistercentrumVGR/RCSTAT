1d4f314b46f4777ddbd015f2de1f4d9c10edc1a7
Mattias Frigren (admin) <admmatfr@vgregion.se>
2023-10-17 14:43:03 +0200

    deploy to prod

--- PsScripts/deploy.ps1
+++ PsScripts/deploy.ps1
@@ -1,11 +1,11 @@
 Write-Output "$(Get-ChildItem *_*.tar.gz -Name)"
-if (-not $Env:R_RCBT_DEPLOY_PATH) {
-    Write-Error "The required environment variable R_RCBT_DEPLOY_PATH has not been set"
+if (-not $Env:R_RCSTAT_DEPLOY_PATH) {
+    Write-Error "The required environment variable R_RCSTAT_DEPLOY_PATH has not been set"
     exit
 }
 Write-Output "$(Get-Location)\$(Get-ChildItem *_*.tar.gz -Name)"
-Remove-Item -Path "$Env:R_RCBT_DEPLOY_PATH\$(Get-ChildItem *_*.tar.gz -Name)" -Force 
-Copy-Item -Path "$(Get-Location)\$(Get-ChildItem *_*.tar.gz -Name)" -Destination "$Env:R_RCBT_DEPLOY_PATH" -Force 
+Remove-Item -Path "$Env:R_RCSTAT_DEPLOY_PATH\$(Get-ChildItem *_*.tar.gz -Name)" -Force 
+Copy-Item -Path "$(Get-Location)\$(Get-ChildItem *_*.tar.gz -Name)" -Destination "$Env:R_RCSTAT_DEPLOY_PATH" -Force 
 $runscript = "$(Get-Location)\PsScripts\start_R_Process.ps1"
 & $runscript 
 Write-Output "Deploy Complete!"
--- PsScripts/start_R_Process.ps1
+++ PsScripts/start_R_Process.ps1
@@ -1,3 +1,3 @@
-Set-Location "$Env:R_RCBT_DEPLOY_PATH"
-Start-Process PowerShell -Verb RunAs "-ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$Env:R_RCBT_DEPLOY_PATH\check_install.ps1';`""
+Set-Location "$Env:R_RCSTAT_DEPLOY_PATH"
+Start-Process PowerShell -Verb RunAs "-ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$Env:R_RCSTAT_DEPLOY_PATH\check_install.ps1';`""
 exit 

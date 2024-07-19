13d7c1110ba95386cbbaf65232f792f760387476
Alexander Thorén <aleth@rcvg.local>
2024-07-19 09:32:32 +0200

    Update API key, add basic test for api functions

---
+++ tests/testthat/test-api.R
@@ -0,0 +1,11 @@
+test_that("api_works", {
+
+  expect_no_error(api_register_meta(102))
+
+  api_register_meta(102) |>
+    expect_type("list") |>
+    expect_length(2) |>
+    names() |>
+    expect_setequal(c("RegisterName", "ShortName"))
+
+})

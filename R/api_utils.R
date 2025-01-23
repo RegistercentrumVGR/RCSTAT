e70c4927f765cfd262704724d2936f13e9d59ae1
Oskar Johansson <oskar.e.johansson@vgregion.se>
2025-01-23 13:12:51 +0000

    Lägger till funktion för att extrahera funktionsanrop till r från api-anrop

--- R/api_utils.R
+++ R/api_utils.R
@@ -13,3 +13,55 @@ get_user_info <- function() {
     "role_id" = scope_role
   )
 }
+
+#' Extract r function call from RC-statisticts API-URL
+#'
+#' @param api_url The URL of a function call to a RC-statistics endpoint.
+#'
+#' @return a character vector of a r function call to a statistics API-endpoint
+#'
+#'@examples
+#' extract_function_call(
+#' api_url = "https://sep.registercentrum.se/stratum/api/statistics/sep/sep-jumbo-antalreg?apikey=bK3H9bwaG4o="
+#' )
+#' @export extract_function_call
+extract_function_call <- function(api_url) {
+
+  api_url <- stringr::str_split_1(
+    string = stringr::str_split_1(
+      api_url,
+      pattern = "statistics/"
+    )[2],
+    pattern = "/"
+  )[2]
+
+  function_name <- stringr::str_split_i(
+    api_url,
+    pattern = "\\?",
+    1
+  )
+
+  function_arguments <-
+    stringr::str_split(
+      stringr::str_split_i(
+        api_url,
+        pattern = "\\?",
+        2
+      ),
+      pattern = "&"
+    )[[1]]
+
+  function_arguments <- function_arguments[-c(grep("apikey", function_arguments))]
+
+  function_call <- paste0(function_name, "(")
+
+  for (argument in function_arguments) {
+    argument <- gsub("=", "='", argument)
+    function_call <- paste0(function_call, argument, "', ")
+  }
+  function_call <- paste0(function_call, ")")
+  function_call <- gsub(", )", ")", function_call)
+
+  return(function_call)
+
+}
